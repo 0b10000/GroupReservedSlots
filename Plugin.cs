dc0154dc0943847d18481dec8817e1bd6c9d7e8a
0b10000 <81607037+0b10000@users.noreply.github.com>
2021-07-28 01:21:44 -0400

    Remove unnecessary usings and debug

--- Plugin.cs
+++ Plugin.cs
@@ -1,15 +1,6 @@
 using System;
 using Exiled.API.Features;
 using HarmonyLib;
-using MapEvents = Exiled.Events.Handlers.Map;
-using PlayerEvents = Exiled.Events.Handlers.Player;
-using Scp049Events = Exiled.Events.Handlers.Scp049;
-using Scp079Events = Exiled.Events.Handlers.Scp079;
-using Scp096Events = Exiled.Events.Handlers.Scp096;
-using Scp106Events = Exiled.Events.Handlers.Scp106;
-using Scp914Events = Exiled.Events.Handlers.Scp914;
-using ServerEvents = Exiled.Events.Handlers.Server;
-using WarheadEvents = Exiled.Events.Handlers.Warhead;
 // ReSharper disable StringLiteralTypo
 
 namespace GroupReservedSlots
@@ -52,12 +43,8 @@ namespace GroupReservedSlots
             {
                 Harmony = new Harmony("0b10000.groupreservedslots");
 
-                var lastDebugStatus = Harmony.DEBUG;
-                Harmony.DEBUG = true;
-                
                 Harmony.PatchAll();
 
-                Harmony.DEBUG = lastDebugStatus;
                 Log.Info("Patched successfully!");
             }
             catch (Exception e)
