d65de9943ac3eb00e6f7b63ed145fdf0fef871f5
zerob10000 <81607037+0b10000@users.noreply.github.com>
2021-07-28 07:36:05 -0400

    remove EventHandlers

--- EventHandlers.cs
+++
@@ -1,8 +0,0 @@
-namespace GroupReservedSlots
-{
-    public class EventHandlers
-    {
-        private readonly Plugin plugin;
-        public EventHandlers(Plugin plugin) => this.plugin = plugin;
-    }
-}
